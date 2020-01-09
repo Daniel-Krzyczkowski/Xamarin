8199059ff2577d4c8dc058e825dfc9effab56212
Daniel Krzyczkowski <daniel.krzyczkowski@hotmail.com>
2020-01-09 06:37:45 +0100

    Update MainActivity.cs

--- Auth0.Xamarin/src/Auth0.Xamarin.Droid/MainActivity.cs
+++ Auth0.Xamarin/src/Auth0.Xamarin.Droid/MainActivity.cs
@@ -48,7 +48,7 @@ namespace Auth0.Xamarin.Droid
 
         private async Task LoginAsync()
         {
-            var loginResult = await _auth0Client.LoginAsync(new { audience = "devisland" });
+            var loginResult = await _auth0Client.LoginAsync();
 
             if (!loginResult.IsError)
             {
